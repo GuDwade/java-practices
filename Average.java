82c0a5ad2eb938e0e112a0e97d87514969477f5e
GuDwade <849467768@qq.com>
2019-10-27 14:06:08 +0800

    提交代码

---
+++ Average.java
@@ -0,0 +1,18 @@
+public  class Average{
+	public static void main(String[] args){
+		if(args.length==0){
+			System.out.println("至少输入一个数字");
+			return;
+		}
+		int sum=0;
+		int i=0;
+		while(i<args.length)
+		{
+		sum=sum+Integer.parseInt(args[i]);
+		i++;
+		}
+		double average=sum/args.length;
+		System.out.println(average);
+	}
+	
+}
