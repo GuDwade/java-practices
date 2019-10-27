3674a5a3eeedba2890f1915d42fdffc2a1d86d7c
GuDwade <849467768@qq.com>
2019-10-27 14:04:38 +0800

    提交代码

---
+++ Practice.java
@@ -0,0 +1,80 @@
+public class Practice{
+	public static void main(String[] args){
+		/*System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[2]);
+		System.out.println(args.length);*/
+		//boolean truth=true;
+		//System.out.println("truth="+truth);
+		//System.out.println(truth+1);
+		//String Gu="gu fu cheng";
+		//System.out.println(Gu);
+		//System.out.println("=============");
+		//字符串拼接
+		//String a="你好";
+		//String b="顾哥";
+		//System.out.println(a+b);
+		//System.out.println("=============");
+		/*if(args.length<2){
+			System.out.println("请输入两个数字");
+			return;
+		}
+			int x=Integer.parseInt(args[0]);
+			int y=Integer.parseInt(args[1]);
+			System.out.printf("%d+%d的结果是%d\n",x,y,(x+y));
+			System.out.println(""+x+"+"+y+"的结果是"+(x+y) );//打印再换行
+		    System.out.print(""+x+"+"+y+"的结果是"+(x+y) );//打印不换行
+			System.out.print("哥哥");
+			
+		    //int x=10;
+			//System.out.println(x);已在主方法中定义X,之后不能定义重名变量，即使类型不同
+			/*{
+			String x="大哥大哥你好吗？";
+            System.out.println(x);		//变量的作用域与{}有关	
+			}
+			int x=10;
+			System.out.println(x);
+			*/
+			/*
+			String s="12345";
+			System.out.println(Integer.parseInt(s));
+			System.out.println(Long.parseLong(s));
+			System.out.println(Double.parseDouble(s));
+			
+			int x=23;
+			long y=33L;
+			double z=3.1415;
+			System.out.println(String.valueOf(x));
+			System.out.println(String.valueOf(y));
+			System.out.println(String.valueOf(z));
+			*/
+			/*System.out.println(-1%3);
+			System.out.println(-5%3);
+			int a=3;
+			int b=33;
+			System.out.println(a<b);//类型为布尔类型
+			System.out.println(a>b);
+			System.out.println(a>b&&a<b);//&&  || 短路特性
+			System.out.println(a>b&a<b);//不短路
+			*/
+		int n=137;
+	    System.out.println((n>>2)&0x7);//取2-4位
+		
+		}
+
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
