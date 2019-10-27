34a65ace82191b13f3e772689a78e76a5ef35c94
GuDwade <849467768@qq.com>
2019-10-27 14:19:57 +0800

    提交代码

--- AverageSum.java
+++ AverageSum.java
@@ -1,8 +1,8 @@
-public  class Average{
+public  class AverageSum{
 	public static void main(String[] args){
 		if(args.length==0){
 			System.out.println("至少输入一个数字");
-			return;
+		return;
 		}
 		int sum=0;
 		int i=0;
