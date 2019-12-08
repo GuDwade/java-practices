493aedc27227b3c901c5addc72ea456b710925f7
GuDwade <849467768@qq.com>
2019-12-08 23:01:38 +0800

    提交代码

---
+++ 2018-12-8/src/Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    public int val;
+    public Node next;
+    Node(int val,Node next){
+        this.val=val;
+        this.next=next;
+    }
+    Node(int val){
+        this(val,null);
+    }
+}
